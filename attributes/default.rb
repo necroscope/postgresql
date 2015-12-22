738f4662a8e0dbd23de329d20f8b42d29706099f
Daniel -The Necroscope <necroscope@users.noreply.github.com>
2015-12-21 22:31:03 -0800

    updated network access

--- attributes/default.rb
+++ attributes/default.rb
@@ -282,10 +282,11 @@ default['postgresql']['pg_hba'] = [
   {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
   {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
   {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
-  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '0.0.0.0/0', :method => 'md5'}
 ]
 
-default['postgresql']['password'] = Hash.new
+default['postgresql']['password'] = 'postgres'
 
 case node['platform_family']
 when 'debian'
